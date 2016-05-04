8bc041e519cf6bdfc881899ec1dac0abadd4cb02
Filipe Barretto <filipe.barretto@kendoo.com.br>
2016-05-03 23:00:44 -0300

    /luiza for delays and error message on format

--- Classes/Roteador.php
+++ Classes/Roteador.php
@@ -146,7 +146,7 @@
 								TelegramConnect::sendMessage($chat_id, "Horário inválido.");
 							}
 						} else {
-							TelegramConnect::sendMessage($chat_id, "Uso: /ida [horario] [vagas] [local] \n Ex: /ida 10:00 2 jardim)
+							TelegramConnect::sendMessage($chat_id, "Uso: /ida [horario] [vagas] [local] \n Ex: /ida 10:00 2 jardim);
 						}
 						break;
 
@@ -184,7 +184,7 @@
 								TelegramConnect::sendMessage($chat_id, "Horário inválido.");
 							}
 						} else {
-							TelegramConnect::sendMessage($chat_id, "Uso: /volta [horario] [vagas] [local] \n Ex: /volta 15:00 2 jardim)
+							TelegramConnect::sendMessage($chat_id, "Uso: /volta [horario] [vagas] [local] \n Ex: /volta 15:00 2 jardim);
 						}
 						break;
 
