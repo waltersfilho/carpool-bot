f29b1a94270022409e9d24b8c1fd2d340caa46c3
Filipe Barretto <filipe.barretto@kendoo.com.br>
2016-05-05 20:49:38 -0300

    Function optimizations and backward compatibility to create carpool

--- code/Classes/Carona.php
+++ code/Classes/Carona.php
@@ -23,7 +23,11 @@
 			error_log($this->spots);
 			error_log($this->location);
 
-			if (isset($this->spots) && isset($this->location)) {
+			error_log("teste");
+			error_log(!empty($this->spots));
+			error_log(!empty($this->location));
+
+			if (!empty($this->spots) && !empty($this->location)) {
 				return "\n" . substr($this->travel_hour, 0, -3) . " - @" . $this->username . "\n" . $this->spots . " vagas (" . $this->location . ")";
 			} else {
 				return "\n" . substr($this->travel_hour, 0, -3) . " - @" . $this->username;
