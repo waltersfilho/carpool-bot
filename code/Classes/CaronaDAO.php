ea6c20260e7554b75d94769c851c9aa4e0cd6ec4
Filipe Barretto <filipebarretto@gmail.com>
2016-05-21 18:26:28 -0300

    update

--- code/Classes/CaronaDAO.php
+++ code/Classes/CaronaDAO.php
@@ -16,10 +16,7 @@
 
 		const QUERY_SEARCH = "SELECT * FROM public.caroneiros WHERE chat_id = :chat_id AND user_id = :user_id AND route = :route::bit(1) ORDER BY timestamp ASC;";
         
-        /*
         const QUERY_LIST_CARPOOLS = "SELECT * FROM public.caroneiros WHERE chat_id = :chat_id AND route = :route::bit(1) ORDER BY timestamp ASC;";
-        */
-        const QUERY_LIST_CARPOOLS = "SELECT * FROM public.caroneiros WHERE chat_id = :chat_id;";
 
 		const QUERY_REMOVE_CARPOOL = "DELETE FROM public.caroneiros WHERE chat_id = :chat_id AND user_id = :user_id AND route = :route::bit(1)";
 
@@ -51,10 +48,7 @@
             
             $this->db->query(CaronaDAO::QUERY_LIST_CARPOOLS);
 			$this->db->bind(":chat_id", $chat_id);
-            
-            /*
             $this->db->bind(":route", route);
-            */
             
             $this->db->execute();
                         
