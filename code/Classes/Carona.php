dbc2bbb93c85ff345cdf83508b6be5aa25f83be0
Filipe Barretto <filipebarretto@gmail.com>
2016-05-21 15:58:00 -0300

    update

--- code/Classes/Carona.php
+++ code/Classes/Carona.php
@@ -22,7 +22,7 @@
 		public function __toString() {
             
             
-            return "\n- @" . $this->username;
+            return "\n" . $this->timestamp . " - @" . $this->username;
                 /*
 			if (!empty($this->spots) && !empty($this->location)) {
 				return "\n" . gmp_strval(gmp_init($this->timestamp)) . " - @" . $this->username . "\n" . $this->spots . " vagas (" . $this->location . ")";
