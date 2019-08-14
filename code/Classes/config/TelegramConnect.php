7386957ca71f5721b11a9936906dea93d8e97fbf
waltersfilho <wspfilho@gmail.com>
2019-08-14 19:20:14 -0300

    Desativando webview preview

--- code/Classes/config/TelegramConnect.php
+++ code/Classes/config/TelegramConnect.php
@@ -4,7 +4,7 @@ class TelegramConnect
 {
     /*TODO make methods non-static, implement constructor*/
     public static function sendMessage ($chatId, $message) {
-        $url = Config::getBotConfig("ApiRequestUrl")."/sendMessage?chat_id=".$chatId."&text=".urlencode($message)."&parse_mode=html";
+        $url = Config::getBotConfig("ApiRequestUrl")."/sendMessage?chat_id=".$chatId."&text=".urlencode($message)."&parse_mode=html&disable_web_page_preview=true";
         file_get_contents($url);
     }
 }
