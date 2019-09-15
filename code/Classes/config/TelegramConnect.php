b7758a71580fbebf8695aa7cb773339a7c25e824
Walter Filho <wspfilho@gmail.com>
2019-09-15 20:09:53 -0300

    Parseando json da response se é admin usando a função decode

--- code/Classes/config/TelegramConnect.php
+++ code/Classes/config/TelegramConnect.php
@@ -10,6 +10,7 @@ class TelegramConnect
     public static function isAdmin ($chatId, $userId) {
         $url = Config::getBotConfig("ApiRequestUrl")."/getChatMember?chat_id=".$chatId."&user_id=".$userId;
         $teste = file_get_contents($url);
-        error_log("Logando response:" . $teste['result']['status']);
+        $obj = json_decode($teste);
+        error_log("Logando response:" . $obj->{'result'}->{'status'});
     }
 }
