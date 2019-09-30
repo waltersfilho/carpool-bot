dc8c632c8caac72c05b6933b43a76fbb783f1f6c
waltersfilho <wspfilho@gmail.com>
2019-09-30 18:23:22 -0300

    Corrigindo caso de mudança de mês, em meses terminando antes do dia 31 (2)

--- code/Classes/dao/CaronaDAO.php
+++ code/Classes/dao/CaronaDAO.php
@@ -16,10 +16,10 @@ class CaronaDAO
     const QUERY_SEARCH = "select * from public.caroneiros where chat_id = :chat_id and user_id = :user_id and route = :route::bit(1) and expired = '0'::bit(1) ORDER BY travel_hour ASC;";
 
     const LISTA_QUERY_IDA_HOJE = "select p.picpay, p.carpool, c.* from public.caroneiros c inner join public.caroneiro_pagamento p on (c.user_id = p.user_id and c.chat_id = p.chat_id) where c.chat_id = :chat_id and route = '0'::bit(1) and expired = '0'::bit(1) and (SELECT EXTRACT(DAY FROM travel_hour)) = (SELECT EXTRACT(DAY FROM now())) ORDER BY travel_hour ASC;";
-    const LISTA_QUERY_IDA_AMANHA = "select p.picpay, p.carpool, c.* from public.caroneiros c inner join public.caroneiro_pagamento p on (c.user_id = p.user_id and c.chat_id = p.chat_id) where c.chat_id = :chat_id and route = '0'::bit(1) and expired = '0'::bit(1) and travel_hour between tomorrow and now() + interval '24 hour' ORDER BY travel_hour ASC;";
+    const LISTA_QUERY_IDA_AMANHA = "select p.picpay, p.carpool, c.* from public.caroneiros c inner join public.caroneiro_pagamento p on (c.user_id = p.user_id and c.chat_id = p.chat_id) where c.chat_id = :chat_id and route = '0'::bit(1) and expired = '0'::bit(1) and travel_hour between TIMESTAMP 'tomorrow' and now() + interval '24 hour' ORDER BY travel_hour ASC;";
 
     const LISTA_QUERY_VOLTA_HOJE = "select p.picpay, p.carpool, c.* from public.caroneiros c inner join public.caroneiro_pagamento p on (c.user_id = p.user_id and c.chat_id = p.chat_id) where c.chat_id = :chat_id and route = '1'::bit(1) and expired = '0'::bit(1) and (SELECT EXTRACT(DAY FROM travel_hour)) = (SELECT EXTRACT(DAY FROM now())) ORDER BY travel_hour ASC;";
-    const LISTA_QUERY_VOLTA_AMANHA = "select p.picpay, p.carpool, c.* from public.caroneiros c inner join public.caroneiro_pagamento p on (c.user_id = p.user_id and c.chat_id = p.chat_id) where c.chat_id = :chat_id and route = '1'::bit(1) and expired = '0'::bit(1) and travel_hour between tomorrow and now() + interval '24 hour' ORDER BY travel_hour ASC;";
+    const LISTA_QUERY_VOLTA_AMANHA = "select p.picpay, p.carpool, c.* from public.caroneiros c inner join public.caroneiro_pagamento p on (c.user_id = p.user_id and c.chat_id = p.chat_id) where c.chat_id = :chat_id and route = '1'::bit(1) and expired = '0'::bit(1) and travel_hour between TIMESTAMP 'tomorrow' and now() + interval '24 hour' ORDER BY travel_hour ASC;";
 
     const QUERY_REMOVE_CARPOOL = "delete from public.caroneiros where chat_id = :chat_id and user_id = :user_id and route = :route::bit(1)";
 
