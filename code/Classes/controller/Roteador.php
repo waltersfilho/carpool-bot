9e57dd5a3b7a053619011eb8962933d80a52d649
Walter Filho <walter.filho@athonenergia.com.br>
2022-03-28 20:30:52 -0300

    Corrigindo parametrização da query de inserçao de regras

--- code/Classes/controller/Roteador.php
+++ code/Classes/controller/Roteador.php
@@ -474,6 +474,8 @@ class Roteador
                     }
                     else if(count($args) == 2 && TelegramConnect::isAdmin($chat_id, $user_id)){
                         $dao->inserirRegra($chat_id, $message);
+
+                        TelegramConnect::sendMessage($chat_id, "Regras inserida/atualizada com sucesso.");
                     }
                     break;
             }
