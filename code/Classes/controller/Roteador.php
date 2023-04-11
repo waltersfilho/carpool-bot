04710523b74544ddfa1ac6c2ace7234db37aa499
Walter Filho <walter.filho@athonenergia.com.br>
2023-04-11 20:47:04 -0300

    test: print args

--- code/Classes/controller/Roteador.php
+++ code/Classes/controller/Roteador.php
@@ -497,7 +497,7 @@ class Roteador
 
                 case 'broadcast':
                     if(TelegramConnect::isBotOwner($chat_id)){
-                        TelegramConnect::sendMessage($chat_id, $args[1]);
+                        TelegramConnect::sendMessage($chat_id, $args);
                     }
                     break;
             }
