aaae556ec7e6e94b7bdc25cfcc5c4134d75ef927
Walter Filho <wspfilho@gmail.com>
2023-04-29 12:36:21 -0300

    Liberando cors

--- code/index.php
+++ code/index.php
@@ -1,4 +1,6 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    
     require_once "Classes/config/Config.php";
     require_once "Classes/controller/Roteador.php";
     require_once "Classes/config/Connection.php";
