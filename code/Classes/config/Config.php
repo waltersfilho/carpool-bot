894bdb255c890aac3392374d7cc3f8bc67367a57
Walter Filho <wspfilho@gmail.com>
2026-09-18 19:13:58 -0300

    Add DBPort configuration with default value

--- code/Classes/config/Config.php
+++ code/Classes/config/Config.php
@@ -12,6 +12,7 @@ class Config{
         self::$botConfigs["ApiKeyTelegram"] = array_key_exists("API_KEY_TELEGRAM", $_ENV) ? $_ENV["API_KEY_TELEGRAM"] : NULL;
         self::$botConfigs["ApiRequestUrl"] = array_key_exists("API_KEY_TELEGRAM", $_ENV) ? 'https://api.telegram.org/bot' . $_ENV["API_KEY_TELEGRAM"] : NULL;
         self::$botConfigs["DBHost"] = array_key_exists("DB_HOST", $_ENV) ? $_ENV["DB_HOST"] : NULL;
+		self::$botConfigs["DBPort"] = array_key_exists("DB_PORT", $_ENV) ? $_ENV["DB_PORT"] : 5432;
         self::$botConfigs["DBUser"] = array_key_exists("DB_USER", $_ENV) ? $_ENV["DB_USER"] : NULL;
         self::$botConfigs["DBPass"] = array_key_exists("DB_PASS", $_ENV) ? $_ENV["DB_PASS"] : NULL;
         self::$botConfigs["DBName"] = array_key_exists("DB_NAME", $_ENV) ? $_ENV["DB_NAME"] : NULL;
